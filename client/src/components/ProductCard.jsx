2b22ddba1a4c257b52dd6a77f433c902e50cb1b9
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-03-14 22:00:58 +0530

    fixing add to cart redux

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { addCart } from "../api/api.cart";
+import { useDispatch, useSelector } from "react-redux";
+import { cartAsync } from "../redux/cartReducer";
 
 function ProductCard({ props, User }) {
   const { image, title, price, _id } = props;
@@ -9,13 +11,14 @@ function ProductCard({ props, User }) {
   const showToast = () => {
     toast.success("Added to your Cart!");
   };
-
+  const d = useDispatch();
 
   const handleAddToCart = async (productID) => {
     // Perform the necessary actions to add the product to the cart
     // For demonstration purposes, let's just show the toast notification
-    const addcart = addCart({userId:User.data.id, productId:productID});
+    const addcart = addCart({ userId: User.data.id, productId: productID });
     console.log(addcart);
+    d(cartAsync(productID));
 
     showToast();
     console.log(`Product added to cart: ${productID}`);
