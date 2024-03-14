3ebfc749571a81635e84451805e8b3e7c601c1cc
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-03-14 17:04:54 +0530

    adding in cart

--- client/src/redux/cartReducer.jsx
+++ client/src/redux/cartReducer.jsx
@@ -7,24 +7,22 @@ export const cartSlice = createSlice({
     data: [],
   },
   reducers: {
-    cart: (state, action) => {
-      state.data = action.payload;
+    addToCart: (state, action) => {
+      state.data.push(action.payload); // Push new data into the data array
     },
   },
 });
 
-export const { cart } = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
 
-export const cartAsync =
-  ({ _id }) =>
-  async (dispatch) => {
-    try {
-      axios
-        .get(`http://localhost:5000/api/v1/products/${_id}`)
-        .then((res) => dispatch(cartSlice.actions.cart(res.data)))
-        .catch((err) => console.log(err));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const cartAsync = (_id) => async (dispatch) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/api/v1/products/${_id}`
+    );
+    dispatch(addToCart(response.data)); // Dispatch action with the response data
+  } catch (error) {
+    console.log(error);
+  }
+};
